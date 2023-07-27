1bb59c8dfee786d7bb7fda87d2dd70ea68d1a9b2
Mateus Ceci <mateusceci@gmail.com>
2023-07-27 13:40:00 -0300

    second commit

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const form = document.querySelector('#form')
-const numberA = document.querySelector('#numberA')
-const numberB = document.querySelector('#numberB')
+const inputA = document.querySelector('#numberA')
+const inputB = document.querySelector('#numberB')
 
-form.addEventListener('submit', function () {
-    if(numberB.value > numberA.value){
+form.addEventListener('submit', function (e) {
+
+
+    e.preventDefault()
+
+    const numberA = parseFloat(inputA.value)
+    const numberB = parseFloat(inputB.value)
+
+   if(numberB > numberA){
         alert('O formulário é valido')
     }
     else {
         alert('Formulário inválido. Número B deve ser maior que número A.')
-    }
+    } 
 } )
